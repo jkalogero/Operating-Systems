a8b79b58fdbd75c1bbd405375ea54c771fb8c6b8
jkalogero <johnkalo22@hotmail.com>
2020-04-24 21:57:55 +0300

    Implement error handlings and use nanosleep.

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -3,11 +3,41 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <signal.h>
+#include <math.h>
+#include <time.h>
+#include <errno.h>
 
 
 int buffer = 0;
 int num_of_proc;
 pid_t pid[1000000];
+int delay[1000000];
+int list_of_signals[35] = { SIGABRT , SIGALRM , SIGBUS, SIGCHLD, SIGCLD, SIGCONT, SIGFPE, SIGHUP, SIGILL, SIGIO, SIGINT, SIGIOT, SIGKILL, SIGPIPE, SIGPOLL, SIGPROF, SIGPWR, SIGQUIT, SIGSEGV, SIGSTKFLT, SIGSTOP, SIGTSTP, SIGSYS, SIGTERM, SIGTRAP, SIGTTIN, SIGTTOU, SIGURG, SIGUSR1, SIGUSR2, SIGVTALRM, SIGXCPU };
+
+
+
+int msleep(long msec)
+{
+    struct timespec ts;
+    int res;
+
+    if (msec < 0)
+    {
+        errno = EINVAL;
+        return -1;
+    }
+
+    ts.tv_sec = msec / 1000;
+    ts.tv_nsec = (msec % 1000) * 1000000;
+
+    do {
+        res = nanosleep(&ts, &ts);
+    } while (res && errno == EINTR);
+
+    return res;
+}
+
+
 
 void child_handler(int sig){  
     if (sig == 10) printf("[Child Process %d: %d] Value: %d!\n", num_of_proc, getpid(), buffer);	
@@ -21,29 +51,45 @@ void child_handler(int sig){
         printf("[Child process %d: %d] Time Expired! Final Value: %d\n", num_of_proc, getpid(), buffer);
         exit(0);
     }
+    else{
+        printf("No action for this signal.\n");
+    }
+    // int remaining = nanosleep(0,0);
+    // printf("remaining =%d\n", remaining);
+    // nanosleep(float(remaining/1), float(remaining%1));
 }
 
 void father_handler(int sig){
-    printf("\n[Father process: %d] Will ask current values (SIGUSR1) from all active children processes.\n", getpid());
     // sleep(10);
     if (sig == 10){
+        printf("\n[Father process: %d] Will ask current values (SIGUSR1) from all active children processes.\n", getpid());
         for (int i = 0; i < num_of_proc; i++) kill(pid[i], SIGUSR1);
     }
     else if (sig == 12){
         printf("[Process %d] Echo!\n", getpid());
     }
-    else if (sig == 15){
+    else if (sig == 15 || sig == 2){
         for (int i = 0; i < num_of_proc; i++){
             printf("[Father process: %d] Will terminate (SIGTERM) child process %d: %d\n", getpid(), i, pid[i]);
             kill(pid[i], SIGTERM);
         }
+    }
+    else{
+        printf("No action for this signal.\n");
     }
 }
 
 
 
 int main(int argc, char** argv) {
-    
+    for (int i = 0; i < argc-1; i++){
+        delay[i] = atoi(argv[i+1]);
+        if (delay[i] <= 0 ){
+            printf("Please provide a positive number for the delays.\n");
+            exit(0);
+        }
+    }
+
     printf("Maximum execution time of children is set to 50 seconds.\n\n");
 
     if (argc < 2){
@@ -55,35 +101,37 @@ int main(int argc, char** argv) {
     // start forking...
     printf("[Father process : %d] Was created and will create %d children.\n", getpid(), num_of_proc);
     for (int i = 0; i < argc -1; i++){
-        int delay = atoi(argv[i+1]);
+        
         pid[i] = fork();
         if (pid[i] == -1) {
             perror("fork");
         } 
         else if (pid[i] == 0) {
             // child's code
-            alarm(50);
+            alarm(15);
             num_of_proc = i+1;
             printf("[Child Process %d: %d] Was created and will pause!\n", num_of_proc, getpid());
-            raise(SIGSTOP);
+            while(raise(SIGSTOP) != 0){}
             printf("[Child Process %d: %d] Is starting!\n", i+1, getpid());
             // struct for sigaction
 	        struct sigaction action;
             action.sa_flags = 0;
             sigemptyset(&action.sa_mask);
+            sigaddset(&action.sa_mask, SIGSTOP);
 	        action.sa_handler=child_handler;
-	        sigaction(SIGUSR1, &action, NULL);
-            sigaction(SIGUSR2, &action, NULL);
-            sigaction(SIGTERM, &action, NULL);
-            sigaction(SIGALRM, &action, NULL);
+            // for (int i = 0; i < 35; i++) sigaction(list_of_signals[i], &action, NULL);
+            if (sigaction(SIGUSR1, &action, NULL) ==  -1) printf("Failed to set up handler for SIGUSR1!");
+            if (sigaction(SIGUSR2, &action, NULL) ==  -1) printf("Failed to set up handler for SIGUSR2!");
+            if (sigaction(SIGTERM, &action, NULL) ==  -1) printf("Failed to set up handler for SIGTERM!");
+            if (sigaction(SIGALRM, &action, NULL) ==  -1) printf("Failed to set up handler for SIGALRM!");
             while(1){
                 buffer++;
-                sleep(delay);
+                // nanosleep(pow(10, 9)*float(delay[i], 0));
+                double nano_delay = (double) delay[i];
+                msleep(nano_delay*pow(10, 3));
+                printf("AAAAAAAAAAAAAAAA\n");
             }
             exit(0);
-        }
-        else{
-            // parent's code
         }
     }
     for(int i=0; i<num_of_proc; i++){
@@ -93,24 +141,26 @@ int main(int argc, char** argv) {
     }
     
     for (int i=0; i<num_of_proc; i++){
-        kill(pid[i], SIGCONT);
+        while(kill(pid[i], SIGCONT) != 0){}
     }
     // struct for sigaction
     struct sigaction action;
     action.sa_flags = 0;
     sigemptyset(&action.sa_mask);
+    sigaddset(&action.sa_mask, SIGSTOP);
     action.sa_handler=father_handler;
-    sigaction(SIGUSR1, &action, NULL);
-    sigaction(SIGUSR2, &action, NULL);
-    sigaction(SIGTERM, &action, NULL);
-    sigaction(SIGTERM, &action, NULL);
+    // for (int i = 0; i < 35; i++) sigaction(list_of_signals[i], &action, NULL);
+    if (sigaction(SIGUSR1, &action, NULL) ==  -1) printf("Failed to set up handler for SIGUSR1!");
+    if (sigaction(SIGUSR2, &action, NULL) ==  -1) printf("Failed to set up handler for SIGUSR2!");
+    if (sigaction(SIGTERM, &action, NULL) ==  -1) printf("Failed to set up handler for SIGTERM!");
+    if (sigaction(SIGINT, &action, NULL) ==  -1) printf("Failed to set up handler for SIGINT!");
     // while(1){}
     // waiting until all my childen die
     for(int i=0; i<num_of_proc; i++){
-       wait(NULL);
+        int ret2, wstatus2;
+        ret2 = waitpid(pid[i], &wstatus2, WNOHANG);
+        while(ret2<=0 || WIFSTOPPED(wstatus2)){ret2 = waitpid(pid[i], &wstatus2, WUNTRACED);}
     }
-    printf("All of my children are now dead...\nExiting...");
+    printf("All of my children are now dead...\nExiting...\n");
     exit(0);
-    
-    // return 0;
 }
